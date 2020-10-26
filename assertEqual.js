cf10809d9aca2b9a688f4b2c5a6f6935846d1091
Monica Li <monicali@live.ca>
2020-10-26 21:28:26 +0000

    added assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertation Passed: ' + actual + ' === ' + expected);
+  } else if (actual !== expected) {
+    console.log('🛑🛑🛑 Assertation Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 4);
