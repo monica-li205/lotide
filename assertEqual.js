e2e756604f9330e356663c85db14a9d2b9636f4c
Monica Li <monicali@live.ca>
2020-10-26 21:39:55 +0000

    used template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅✅✅ Assertation Passed: ' + actual + ' === ' + expected);
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log('🛑🛑🛑 Assertation Failed: ' + actual + ' !== ' + expected);
+    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 
