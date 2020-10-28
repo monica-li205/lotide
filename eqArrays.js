1b1fc2eaed840522fe0480209dd7ec1893aba73b
Monica Li <monicali@live.ca>
2020-10-28 01:48:15 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(firstArray, secArray) {
+  let match = false;
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] === secArray[i]) {
+      match = true;
+    } else if (firstArray[i] !== secArray[i]) {
+      match = false;
+    }
+  }
+  return match;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
