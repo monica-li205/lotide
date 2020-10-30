f0a124215117f8f34b08895942123941ceb5e361
Monica Li <monicali@live.ca>
2020-10-30 04:22:00 +0000

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (objects, callback) => {
+  
+  for (let property in objects) {
+    if (callback(objects[property])) {
+      return property;
+    }
+  }
+  return undefined;
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma'); // => "noma"
